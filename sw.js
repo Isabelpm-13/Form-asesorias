96b4a677000d3a8330b0476649fec2f71f6097f3
Isabelpm-13 <84946083+Isabelpm-13@users.noreply.github.com>
2021-12-10 23:32:49 -0600

    v.5.0

--- sw.js
+++ sw.js
@@ -1,26 +1,32 @@
+//sw
+
 let staticCache = 'staticCache@v1';
 let dynamicCache = 'dynamicCache@v1';
 let immutableCache = 'immutableCache@v1';
 
 self.addEventListener('install', event => {
     console.log("SW Install");
+const _files = [
+    "/index.html",
+    "/style.css",
+    "/js/indexedDB.js",
+     "/js/app.js",
+     "manifest.json",
+     "sw.js",
+     "/assets/icons/noconnection.png",
+     "/assets/icons/icon-192x192.png",
+     "/assets/icons/icon-256x256.png",
+     "/assets/icons/icon-384x384.png",
+     "/assets/icons/icon-512x512.png",
 
-    const _files = [
-      "/index.html",
-      "/style.css",
-      "/js/indexedDB.js",
-      "/assets/icons/noconnection.png",
-       "/js/app.js",
- 
-    ];
-  
-    const _IMMUTABLE_FILES = [
-      "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
-      "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
-    ];
-  
+  ];
 
-    const saveStaticCache = caches.open(staticCache)
+  const _IMMUTABLE_FILES = [
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js",
+    "https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css",
+  ];
+
+const saveStaticCache = caches.open(staticCache)
         .then((cache) => cache.addAll(_files));
 
     const saveImmutableCache = caches.open(immutableCache)
@@ -29,36 +35,68 @@ self.addEventListener('install', event => {
     event.waitUntil(Promise.all([saveStaticCache, saveImmutableCache]));
 });
 
-//Actualizar la caché
-self.addEventListener('activate', (e) => {
-    e.waitUntil(
-        caches.keys().then(cacheList => {
-            return Promise.all(
-                cacheList.map(cache => { 
-                    if (!staticCache.includes(cache) && !immutableCache.includes(cache)) {
-                        return caches.delete(cache);
+self.addEventListener('activate', e => {
+
+    const respuesta = caches.keys().then( keys => {
+
+        keys.forEach( key => {
+
+            if (  key !== staticCache && key.includes('static') ) {
+                return caches.delete(key);
+            }
+
+            if (  key !== dynamicCache && key.includes('dynamic') ) {
+                return caches.delete(key);
+            }
+
+        });
+
+    });
+
+    e.waitUntil( respuesta );
+
+});
+
+
+
+self.addEventListener( 'fetch', e => {
+
+
+    const respuesta = caches.match( e.request ).then( res => {
+
+        if ( res ) {
+            return res;
+        } else {
+
+            return fetch( e.request ).then( newRes => {
+
+                return updateCacheDynamic( dynamicCache, e.request, newRes );
+
+                function updateCacheDynamic(dynamicCache, req, res){
+
+                    if (res.ok) {
+                        caches.open( dynamicCache).then(cache => {
+                            cache.put ( req, res.clone());
+                
+                            return res.clone();
+                        })
+                    } else {
+                        return res;
                     }
-                }));
-        })
-    );
+                }
+
+            });
+
+        }
+
+    });
+
+
+
+    e.respondWith( respuesta );
+
 });
 
 
-
-self.addEventListener("message", (msgClient) => {
-  if (msgClient.data.action == "skipWaiting") {
-    self.skipWaiting();
-  }
-});
-
-//3 Cache First
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) return cachedResponse;
-      return fetch(event.request);
-    })
-  );
-});
 
 
